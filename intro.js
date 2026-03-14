7d517c2ea9458de04303b6d9ac8f8b1211aeea64
Talleve Milan <tallevemilan157@gmail.com>
2026-03-14 11:48:48 +0100

    add intro.js with my info

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Adebiyi Adeyinka"; 
+const age = 17; 
+const favoriteLanguage = "CSS"; 
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Favorite Programming Language: ${favoriteLanguage}`);
